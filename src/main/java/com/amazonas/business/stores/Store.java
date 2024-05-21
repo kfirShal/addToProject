41ecb4392d6d7d1ba7849e4c5122bca0dd887601
Shoham <shohambr@post.bgu.ac.il>
2024-05-21 18:21:06 +0300

    added some func in store

--- src/main/java/com/amazonas/business/stores/Store.java
+++ src/main/java/com/amazonas/business/stores/Store.java
@@ -2,7 +2,9 @@ package com.amazonas.business.stores;
 
 import com.amazonas.business.inventory.Product;
 import com.amazonas.business.stores.purchasePolicy.PurchasePolicy;
+import org.springframework.security.core.parameters.P;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class Store {
@@ -14,6 +16,33 @@ public class Store {
 
 
     //functions
+
+    public  boolean updateProduct(Product toUpdate){
+        Iterator it = products.iterator();
+        while(it.hasNext()){
+            Product curr = (Product) it.next();
+            if(curr.productID() == toUpdate.productID()){
+                products.remove(curr);
+                products.add(toUpdate);
+                return true
+            }
+        }
+        return false;
+    }
+    public  boolean addProduct(Product toAdd){
+        if(products.contains(toAdd)){
+            return false;
+        }
+        products.add(toAdd);
+        return true;
+    }
+    public  boolean removeProduct(Product toRemove){
+        if(products.contains(toRemove)){
+            products.remove(toRemove);
+            return true;
+        }
+        return false;
+    }
     public List<Product> searchProduct(SearchRequest request) {
         return null;
     }
