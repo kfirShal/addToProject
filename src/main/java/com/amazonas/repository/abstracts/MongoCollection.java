c153803bb7b01fdd9ff45fbfbf4c4a04f425fca7
Yuval Roth <rothyuv@post.bgu.ac.il>
2024-05-30 20:39:40 +0300

    removed interface methods from MongoCollection

--- src/main/java/com/amazonas/repository/abstracts/MongoCollection.java
+++ src/main/java/com/amazonas/repository/abstracts/MongoCollection.java
@@ -8,14 +8,5 @@ import java.util.List;
 
                                  //TODO: Uncomment the MongoRepository
 public interface MongoCollection<T>  /*extends MongoRepository<T,String>*/ {
-
-    //TODO: REMOVE THE INTERFACE METHODS
-
-    <S extends T> S insert(S entity);
-
-    <S extends T> List<S> insert(Iterable<S> entities);
-
-    <S extends T> List<S> findAll(Example<S> example);
-
-    <S extends T> List<S> findAll(Example<S> example, Sort sort);
+    
 }
