cdab0528552e4e6656e41dd2f03197cc56e29872
morshay1 <102467340+morshay1@users.noreply.github.com>
2024-06-12 16:21:55 +0300

    Delete src/test/java/com/amazonas/acceptanceTests directory

--- src/test/java/com/amazonas/acceptanceTests/AuthenticationBridge.java
+++
@@ -1,9 +0,0 @@
-package com.amazonas.acceptanceTests;
-
-public interface AuthenticationBridge {
-    boolean authenticate(String userId, String password);
-
-    void testAuthenticateValidUser();
-    void testAuthenticateInvalidUser();
-    void testAuthenticateInvalidPassword();
-}
--- src/test/java/com/amazonas/acceptanceTests/PaymentBridge.java
+++
@@ -1,8 +0,0 @@
-package com.amazonas.acceptanceTests;
-
-import com.amazonas.business.payment.PaymentMethod;
-
-public interface PaymentBridge {
-    boolean charge(PaymentMethod paymentMethod, double amount);
-
-}
