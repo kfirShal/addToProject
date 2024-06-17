5ff3bdf39d2bb74d7c76642701180f758fe151a8
Yuval Roth <rothyuv@post.bgu.ac.il>
2024-06-17 15:47:54 +0300

    fixed external services service

--- backend/src/main/java/com/amazonas/service/ExternalServicesService.java
+++ backend/src/main/java/com/amazonas/service/ExternalServicesService.java
@@ -6,6 +6,7 @@ import com.amazonas.business.permissions.proxies.MarketProxy;
 import com.amazonas.business.shipping.ShippingServiceController;
 import com.amazonas.common.utils.JsonUtils;
 import com.amazonas.common.utils.Response;
+import com.amazonas.service.requests.Request;
 import com.amazonas.service.requests.payment.PaymentServiceManagementRequest;
 import com.amazonas.service.requests.shipping.ShipmentRequest;
 import com.amazonas.service.requests.shipping.ShippingServiceManagementRequest;
@@ -25,49 +26,58 @@ public class ExternalServicesService {
 
     //TODO: USE PROXY
 
-    public String sendShipment(String json){
-        ShipmentRequest request = JsonUtils.deserialize(json, ShipmentRequest.class);
-        return new Response(shippingServiceController.sendShipment(request)).toJson();
+    public String sendShipment(String json) {
+        Request request = Request.from(json);
+        ShipmentRequest shipmentRequest = JsonUtils.deserialize(request.payload(), ShipmentRequest.class);
+        return new Response(shippingServiceController.sendShipment(shipmentRequest)).toJson();
     }
 
-    public String processPayment(String json){
-        PaymentRequest request = JsonUtils.deserialize(json, ShipmentRequest.class);
-        return new Response(paymentServiceController.processPayment(request)).toJson();
+    public String processPayment(String json) {
+        Request request = Request.from(json);
+        PaymentRequest paymentRequest = JsonUtils.deserialize(request.payload(), PaymentRequest.class);
+        return new Response(paymentServiceController.processPayment(paymentRequest)).toJson();
     }
 
     public String addShippingService(String json) {
-        ShippingServiceManagementRequest request = JsonUtils.deserialize(json, ShippingServiceManagementRequest.class);
-        shippingServiceController.addShippingService(request.serviceId(), request.shippingService());
+        Request request = Request.from(json);
+        ShippingServiceManagementRequest serviceRequest = JsonUtils.deserialize(request.payload(), ShippingServiceManagementRequest.class);
+        shippingServiceController.addShippingService(serviceRequest.serviceId(), serviceRequest.shippingService());
         return Response.getOk();
     }
 
     public String removeShippingService(String json) {
-        ShippingServiceManagementRequest request = JsonUtils.deserialize(json, ShippingServiceManagementRequest.class);
-        shippingServiceController.removeShippingService(request.serviceId());
+        Request request = Request.from(json);
+        ShippingServiceManagementRequest serviceRequest = JsonUtils.deserialize(request.payload(), ShippingServiceManagementRequest.class);
+        shippingServiceController.removeShippingService(serviceRequest.serviceId());
         return Response.getOk();
     }
 
     public String updateShippingService(String json) {
-        ShippingServiceManagementRequest request = JsonUtils.deserialize(json, ShippingServiceManagementRequest.class);
-        shippingServiceController.updateShippingService(request.serviceId(), request.shippingService());
+        Request request = Request.from(json);
+        ShippingServiceManagementRequest serviceRequest = JsonUtils.deserialize(request.payload(), ShippingServiceManagementRequest.class);
+        shippingServiceController.updateShippingService(serviceRequest.serviceId(), serviceRequest.shippingService());
         return Response.getOk();
     }
 
     public String addPaymentService(String json) {
-        PaymentServiceManagementRequest request = JsonUtils.deserialize(json, PaymentServiceManagementRequest.class);
-        paymentServiceController.addPaymentService(request.serviceId(), request.paymentService());
+        Request request = Request.from(json);
+        PaymentServiceManagementRequest serviceRequest = JsonUtils.deserialize(request.payload(), PaymentServiceManagementRequest.class);
+        paymentServiceController.addPaymentService(serviceRequest.serviceId(), serviceRequest.paymentService());
         return Response.getOk();
     }
 
     public String removePaymentService(String json) {
-        PaymentServiceManagementRequest request = JsonUtils.deserialize(json, PaymentServiceManagementRequest.class);
-        paymentServiceController.removePaymentService(request.serviceId());
+        Request request = Request.from(json);
+        PaymentServiceManagementRequest serviceRequest = JsonUtils.deserialize(request.payload(), PaymentServiceManagementRequest.class);
+        paymentServiceController.removePaymentService(serviceRequest.serviceId());
         return Response.getOk();
     }
 
     public String updatePaymentService(String json) {
-        PaymentServiceManagementRequest request = JsonUtils.deserialize(json, PaymentServiceManagementRequest.class);
-        paymentServiceController.updatePaymentService(request.serviceId(), request.paymentService());
+        Request request = Request.from(json);
+        PaymentServiceManagementRequest serviceRequest = JsonUtils.deserialize(request.payload(), PaymentServiceManagementRequest.class);
+        paymentServiceController.updatePaymentService(serviceRequest.serviceId(), serviceRequest.paymentService());
         return Response.getOk();
     }
+
 }
